39b615d0f2b2a2842103b8cd8ad4d73858100e79
Paul Dolden <paul@dolden.co.uk>
2023-02-24 20:58:02 +0000

    fix search

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { api } from "../config/api"
-  let value = ""
   let searchQuery = ""
   let searchType = "movie"
 
   async function handleSearch() {
     try {
-      const response = await api.get(`.netlify/search/${searchType}`, {
+      const response = await api.get(`.netlify/search`, {
         params: {
-          query: searchQuery
+          query: searchQuery,
+          type: searchType
         }
       })
       console.log(response)
@@ -20,7 +20,7 @@
 
 <div class="form-control">
   <div class="input-group">
-    <input bind:value={value} type="text" placeholder="Search…" class="input input-bordered" />
+    <input bind:value={searchQuery} type="text" placeholder="Search…" class="input input-bordered" />
     <button class="btn btn-square bg-primary focus:bg-primary-focus" on:click={handleSearch}>
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="black"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
     </button>
