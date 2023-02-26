2725a09cd45ea927f2050688dc26c77c3b380d11
Paul Dolden <paul@dolden.co.uk>
2023-02-26 21:19:59 +0000

    add links to images

--- src/routes/details/[id]/+page.svelte
+++ src/routes/details/[id]/+page.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+  import type { Movie } from "../../../interfaces/Movies";
+  import type { Show } from "../../../interfaces/Shows";
+  import { onMount } from "svelte";
 
+  export let result: Movie | Show
 
+  onMount(() => {
+    console.log(result);
+  });
 </script>
