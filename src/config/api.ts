65d23cc35929e578344e165e88a70726538d6189
Paul Dolden <paul@dolden.co.uk>
2023-03-01 20:04:30 +0000

    fix endpoint

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseUrl = import.meta.env.DEV ? import.meta.env.VITE_SERVICE_URL_DEV : import.meta.env.VITE_SERVICE_URL_PROD;
 
 export const api = axios.create({
-  baseURL: baseUrl,
+  baseURL: baseUrl + '/.netlify/functions/',
   headers: {
     'Content-Type': 'application/json',
   },
