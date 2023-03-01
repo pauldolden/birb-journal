35e1257691defa33df6964ed36be0895e5411db1
Paul Dolden <paul@dolden.co.uk>
2023-03-01 20:01:38 +0000

    updates

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = import.meta.env.DEV ? 'http://localhost:9999/.netlify/functions' : `https://birbjournal.netlify.app/.netlify/functions`;
+const baseUrl = import.meta.env.DEV ? import.meta.env.VITE_SERVICE_URL_DEV : import.meta.env.VITE_SERVICE_URL_PROD;
 
 export const api = axios.create({
   baseURL: baseUrl,
