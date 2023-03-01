7b57f2b53e152b145d81c3645f9a6b746ab52ed0
Paul Dolden <paul@dolden.co.uk>
2023-03-01 21:20:52 +0000

    commit

--- src/routes/details/[id]/+page.svelte
+++ src/routes/details/[id]/+page.svelte
@@ -2,14 +2,16 @@
   import type { Movie } from "../../../interfaces/Movies";
   import type { Show } from "../../../interfaces/Shows";
   import DetailsCard from "../../../components/DetailsCard.svelte";
+	import type { Rating } from "../../../interfaces/Rating";
 
   interface Data {
     result: Show | Movie;
+    rating: Rating;
   }
 
   export let data: Data
 </script>
 
-<DetailsCard result={data.result} />
+<DetailsCard result={data.result} rating={data.rating} />
 
 
